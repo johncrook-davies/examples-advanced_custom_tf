f38e2b5dee241af29ea7d15b6a6e1849db8e39ab
Johnathan Crook-Davies <johnathandavies@me.com>
2021-06-17 17:49:46 +0100

    Added wrapper function and class implementation od Huber loss.

--- custom-loss-functions/custom-loss-functions.md
+++ custom-loss-functions/custom-loss-functions.md
@@ -5,12 +5,17 @@ Custom functions must:
 * Accept two parameters,
   - y_true, the true values, and
   - y_pred, the predicted values
+Custom functions can optionally:
+* Accept parameters to customise the function by using a wrapper function
+* Be defined as a class
 
 As an example, a custom loss function can be written for the Huber loss:
 * for absolute difference (`a`) less than or equal to the threshold `delta`, the loss is `a**2/2`
 * otherwise the loss is equal to the absolute value of `a` minus `delta/2`
 
 ```py
+  # As a function
+  ## With no hyperparameters
   def huber_loss(y_true,y_pred):
     delta = 1
     a = y_true - y_pred
@@ -18,6 +23,28 @@ As an example, a custom loss function can be written for the Huber loss:
     small_error_loss = tf.square(a) / 2
     big_error_loss = delta * (tf.abs(a) - .5 * delta)
     return tf.where(is_small_error, small_error_loss, big_error_loss)
+  ## With hyperparameters
+  def huber_loss(delta):
+    def _huber_loss(y_true,y_pred):
+      a = y_true - y_pred
+      is_small_error = tf.abs(a) <= delta
+      small_error_loss = tf.square(a) / 2
+      big_error_loss = delta * (tf.abs(a) - .5 * delta)
+      return tf.where(is_small_error, small_error_loss, big_error_loss)
+    return _huber_loss
+  # As a class
+  from tensorflow.keras.losses import Loss
+  class HuberLoss(Loss):
+    delta = 1
+    def __init__(self, delta):
+      super().__init__()
+      self.delta = delta
+    def call(self,y_true,y_pred):
+      a = y_true - y_pred
+      is_small_error = tf.abs(a) <= self.delta
+      small_error_loss = tf.square(a) / 2
+      big_error_loss = self.delta * (tf.abs(a) - .5 * self.delta)
+      return tf.where(is_small_error, small_error_loss, big_error_loss)
 ```
 
 This function can then be used in tf by passing the name as a string or reference into `model.compile`.
--- custom-loss-functions/huber.py
+++ custom-loss-functions/huber.py
@@ -2,13 +2,37 @@ import tensorflow as tf
 import numpy as np
 from tensorflow import keras
 
-def my_huber_loss(y_true, y_pred):
-    threshold = 1
-    error = y_true - y_pred
-    is_small_error = tf.abs(error) <= threshold
-    small_error_loss = tf.square(error) / 2
-    big_error_loss = threshold * (tf.abs(error) - (0.5 * threshold))
+# As a function
+## With no hyperparameters
+def huberloss_func(y_true,y_pred):
+    delta = 1
+    a = y_true - y_pred
+    is_small_error = tf.abs(a) <= delta
+    small_error_loss = tf.square(a) / 2
+    big_error_loss = delta * (tf.abs(a) - .5 * delta)
     return tf.where(is_small_error, small_error_loss, big_error_loss)
+## With hyperparameters
+def huberloss_wrap(delta):
+    def _huber_loss(y_true,y_pred):
+      a = y_true - y_pred
+      is_small_error = tf.abs(a) <= delta
+      small_error_loss = tf.square(a) / 2
+      big_error_loss = delta * (tf.abs(a) - .5 * delta)
+      return tf.where(is_small_error, small_error_loss, big_error_loss)
+    return _huber_loss
+# As a class
+from tensorflow.keras.losses import Loss
+class HuberLossClass(Loss):
+    delta = 1
+    def __init__(self, delta):
+      super().__init__()
+      self.delta = delta
+    def call(self,y_true,y_pred):
+      a = y_true - y_pred
+      is_small_error = tf.abs(a) <= self.delta
+      small_error_loss = tf.square(a) / 2
+      big_error_loss = self.delta * (tf.abs(a) - .5 * self.delta)
+      return tf.where(is_small_error, small_error_loss, big_error_loss)
 
 if __name__ == '__main__':
     # Dummy absolute basic inputs and labels with a y = 2x + 1 relationship
@@ -21,8 +45,20 @@ if __name__ == '__main__':
     model.fit(xs, ys, epochs=500,verbose=0)
     print(model.predict([10.0]))
 
-    # Next train model on custom Huber loss for comparison
+    # Next train model on custom Huber loss for comparison using the function api
     model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
-    model.compile(optimizer='sgd', loss=my_huber_loss)
+    model.compile(optimizer='sgd', loss=huberloss_func)
+    model.fit(xs, ys, epochs=500,verbose=0)
+    print(model.predict([10.0]))
+
+    # ... And the wrapper function api
+    model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
+    model.compile(optimizer='sgd', loss=huberloss_wrap(delta=1))
+    model.fit(xs, ys, epochs=500,verbose=0)
+    print(model.predict([10.0]))
+
+    # ... And the class api
+    model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
+    model.compile(optimizer='sgd', loss=HuberLossClass(delta=1))
     model.fit(xs, ys, epochs=500,verbose=0)
     print(model.predict([10.0]))
